b23120a0fb1c3a6f458166a17f374756e193a0c5
Nikita Tsipelev <npometall@mail.ru>
2023-04-07 19:37:07 +0300

    fix-19

--- problems/019-search-anagrams.js
+++ problems/019-search-anagrams.js
@@ -12,7 +12,23 @@
  * @returns {string}
  */
 function searchAnagrams(value) {
-    return undefined;
+  const str = value.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, "").split(" ");
+
+  const anagram1 = [];
+  const anagram2 = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const sortStr = str[i].toLocaleLowerCase().split("").sort().join("");
+    for (let j = i + 1; j < str.length; j++) {
+      if (sortStr === str[j].toLowerCase().split("").sort().join("")) {
+        anagram1.push(str[i]);
+        anagram2.push(str[j]);
+      }
+    }
+  }
+
+  const result = anagram1.concat(anagram2).join(" ");
+  return result;
 }
 
 module.exports = searchAnagrams;
